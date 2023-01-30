19869b90a4642b92d0b4ee43d4af245e3e7f79c1
Brad DerManouelian <bdermanouelian@allego.com>
2023-01-29 18:21:12 -0600

    compile for es5

--- build/app.js
+++ build/app.js
@@ -3,5 +3,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const service_1 = __importDefault(require("./src/service"));
+var service_1 = __importDefault(require("./src/service"));
 exports.default = service_1.default;
